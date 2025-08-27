75f5b0da178ef0ab6aa96df239d51251e67d7b59
Lyubov Davletshina <chernonosova.lyubov@yandex.ru>
2025-08-27 10:56:22 +0300

    final edits

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from masks import get_mask_card_number, get_mask_account
-
-from widget import mask_account_card, get_date
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 print(get_mask_card_number(7000792289606361))
 
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
@@ -19,7 +19,7 @@ def mask_account_card(account_card: str) -> str:
         modified_number_card = get_mask_account(int(number_card))
         new_account_card += modified_number_card
 
-    return new_account_card
+    return str(new_account_card)
 
 
 def get_date(date: str) -> str:
